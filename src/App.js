97ab582c236ff1cecc353bcef38cae0626a2c7fe
Mateor13 <mateo.torres@epn.edu.ec>
2024-07-17 23:19:35 -0500

    footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Btn from './componentes/boton.js';
 import Txt from './componentes/texto.js';
 import Messi from './imagenes/a3a34890-7f03-11ed-a7e9-6563dc325d4d.jpeg';
+import Pie from './componentes/pie.js'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Txt/>
         <img src={Messi} className='Messi' alt='messi'/>
         <Btn/>
+        <Pie/>
       </header>
     </div>
   );
---
+++ src/componentes/pie.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function Pie() {
+    return(
+        <div>
+            <p className='pie'>Messi campeón del Mundo 2022</p>
+            <p className='pie'>Derechos reservados 2024</p>
+        </div>
+    );
+}
+export default Pie;
